eaf84eb6a31fefdf8bd8a530877a62f89ca303c7
Aadi <Aadikalra8@gmail.com>
2025-10-08 01:27:34 +0530

    Update PostOrder.java

--- algorithms/tree/traversal/java/PostOrder.java
+++ algorithms/tree/traversal/java/PostOrder.java
@@ -1,3 +1,9 @@
+/*
+
+Complexity Analysis:
+Time Complexity: O(n), where n is the number of nodes in the tree, as we visit each node exactly once.
+Space Complexity: O(h), where h is the height of the tree. This is for the memory on the recursion stack. In the worst case of a skewed tree, this can be O(n).
+*/
 public class PostOrder {
 
     // Definition of a tree node
