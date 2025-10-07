9de00e1347b9b0a8b6551d206177ff7c0be6fe28
Praneel Dev <devpraneel@gmail.com>
2025-10-08 01:12:39 +0530

    Create IntegerToRoman.java
    Created file at appropriate location, defined functions.

---
+++ algorithms/math/integer_to_roman/IntegerToRoman.java
@@ -0,0 +1,14 @@
+public class IntegerToRoman {
+
+  //add logic explaining conversion scheme.
+
+  public static String intToRoman(int num){} //function for conversion
+
+  public static void main(String[] args){
+
+    //main function
+    
+  }
+
+  
+}
