f078ccb5bc5eaefcbeda64a267333442e3af8ac3
UniqueKJ <kru.t.ika04mjoshi1713@gmail.com>
2025-10-13 10:17:52 +0530

    refactor: reuse existing UnionFind class to avoid duplication

--- algorithms/graph/kruskal_mst/python/kruskal.py
+++ algorithms/graph/kruskal_mst/python/kruskal.py
@@ -1,3 +1,4 @@
+from data_structures.UnionFind.python.union_find import UnionFind
 """
 Kruskal's Algorithm Implementation in Python
 
@@ -5,44 +6,6 @@ Finds the Minimum Spanning Tree (MST) of a connected, undirected,
 weighted graph. Uses Union-Find (Disjoint Set) to detect cycles efficiently.
 
 """
-
-
-class UnionFind:
-    """
-    Union-Find (Disjoint Set) data structure with path compression and union
-    by rank. Used to detect cycles efficiently.
-    """
-
-    def __init__(self, n):
-        self.parent = list(range(n))
-        self.rank = [0] * n
-
-    def find(self, x):
-        """Finds the root of x with path compression."""
-        if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
-
-    def union(self, x, y):
-        """
-        Unions the sets containing x and y. Returns False if x and y are
-        already connected (cycle detected), True otherwise.
-        """
-        root_x = self.find(x)
-        root_y = self.find(y)
-
-        if root_x == root_y:
-            return False
-
-        if self.rank[root_x] < self.rank[root_y]:
-            self.parent[root_x] = root_y
-        elif self.rank[root_x] > self.rank[root_y]:
-            self.parent[root_y] = root_x
-        else:
-            self.parent[root_y] = root_x
-            self.rank[root_x] += 1
-
-        return True
 
 
 def kruskal(n, edges):
